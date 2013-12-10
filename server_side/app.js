ab34372fbb25e94957dd9f24ee5831e9de640169
Alexio <alexioamota@gmail.com>
2013-12-10 04:34:57 +0000

    Some House Cleaning

--- server_side/app.js
+++ server_side/app.js
@@ -1,81 +1,60 @@
-var http = require('http'),
+var express  = require('express'),
+    app = express(),
+    wolfram = require('wolfram-alpha').createClient('G4A8VK-WRV3WA595H'),
     fs = require('fs'),
-    sys = require('sys'),
-    exec = require('child_process').exec,
-    wolfram = require('wolfram-alpha').createClient('G4A8VK-WRV3WA595H',opts),
-    EventEmitter = require('events').EventEmitter,
-    queue = new EventEmitter;
+    exec = require('child_process').exec;
+   // tesseract = require('tesseract'),
+   // tess = tesseract.BaseApi();
+ 
+//Express's powerful parser
+app.use(express.bodyParser())
+ 
+//The upload picture request handler
+app.post('/', function(req, res){
+  //This debugging meassage displays all the info that comes with the file
+  console.log("Received file:\n" + JSON.stringify(req.files));
+ 
+  //Set the directory names
+  var photoName = req.files.image.name;
+  console.log(" Photo: " + photoName);
+  var command = "tesseract " + req.files.image.path + " /tmp/test_out -psm 7";
+  //command = "tesseract /tmp/sample.tiff /tmp/sample -psm 7";
+  child = exec(command, function(error, stdout, stderr){
+          if (error) throw error;
+                console.log('stdout: ' + stdout);
+                console.log('stderr: ' + stderr);
+                fs.readFile('/tmp/test_out.txt', function(err,str){
+                        if (err) throw err;
+                        console.log('File: ' +str);
+                        var answer = '';
+                        var equation = '';
+                        
 
-var writeFile = function writeFile(data){
-	fs.writeFile('test.tif', data , function(err){
-		if (err) throw err;
-		child = exec('tesseract test.tif test', function(error, stdout, stderr){
-			if (error) throw error;
-			
-			console.log('stdout: ' + stdout);
-			console.log('stderr: ' + stderr);
-		});
-	});
-}
+                        wolfram.query(String(str), function (err, result) {
+                                  if (err) throw err;
+                                    
+                                if(result.length != 0){
+                                        console.log("WOLFRAM Result: %j", result);
+                                        answer = result[1].subpods[0].text;
+                                        equation = result[0].subpods[0].text;
+                                }
+                                
 
-queue.on('write', writeFile);
-console.log('Starting...');
-http.createServer(function(req, res){
-	
-	console.log(req.files);
-	
-	var body = '';
-	var last = '';
-	req.on('data', function(chunk){
-		//console.log("Chunk111111111111111111111111: " + chunk);
-		last = chunk;
-		body+=chunk;
-	});
-
-	req.on('end', function(){
-		var filename = 'test.tiff';
-		//filename = 'test.txt';
-        	fs.writeFile(filename, body , function(err){
-                		if (err) throw err;
-                		//console.log("Lastin: " + last);
-				
-				var junkIndex = last.indexOf("--");
-				var junkStr = last.substring(junkIndex);
-				console.log("JUNK!: " + junkStr);
-				body.replace(junkstr, '');
-				//console.log(body);
-
-				var command = 'tesseract /Github/Github/PhotoDIstance-PhotoEquation/server_side/test.tiff /Github/Github/PhotoDIstance-PhotoEquation/server_side/test -psm 7';
-				//command = 'ls';
-                		child = exec(command, function(error, stdout, stderr){
-                        		if (error) throw error;
-
-                        		//console.log('stdout: ' + stdout);
-                        		//console.log('stderr: ' + stderr);
-
-					fs.readFile('test.txt', function(err,str){
-						if (err) throw err;
-						console.log('File: ' +str);
-						
-						//call wolfram to do the math
-						wolfram.query("", function(err, result){
-							if (err) throw err;
-							console.log("Result: ");
-						});
-						
-						res.writeHeader(200, {"Content-Type":"application/json"});
-        					//res.write();
-						var result = {
-							eq: '1+1',
-							ans: 2
-						};
-						res.write(JSON.stringify(result));
-        					//res.end(JSON.stringify(result));
-						res.end();
-						console.log("SENT RESPONSE!");
-					});
-                		});
-        	});
-	});
-
-}).listen(8988);
+                                res.writeHeader(200, {"Content-Type":"application/json"});
+                                       //res.write();
+                                var result = [{
+                                               problem: equation,
+                                        answer: answer
+                                        }];
+                                console.log(JSON.stringify(result));
+                                res.write(JSON.stringify(result));
+                                //res.end(JSON.stringify(result));
+                                res.end();
+                                console.log("SENT RESPONSE!");
+                        });
+                });
+  });
+  //We use Node's FileSystem to rename the file, which actually moves it from the /tmp/ folder it goes to on Linux
+});
+ 
+app.listen(8999);
---
+++ server_side/old_app.js
@@ -0,0 +1,81 @@
+var http = require('http'),
+    fs = require('fs'),
+    sys = require('sys'),
+    exec = require('child_process').exec,
+    wolfram = require('wolfram-alpha').createClient('G4A8VK-WRV3WA595H',opts),
+    EventEmitter = require('events').EventEmitter,
+    queue = new EventEmitter;
+
+var writeFile = function writeFile(data){
+	fs.writeFile('test.tif', data , function(err){
+		if (err) throw err;
+		child = exec('tesseract test.tif test', function(error, stdout, stderr){
+			if (error) throw error;
+			
+			console.log('stdout: ' + stdout);
+			console.log('stderr: ' + stderr);
+		});
+	});
+}
+
+queue.on('write', writeFile);
+console.log('Starting...');
+http.createServer(function(req, res){
+	
+	console.log(req.files);
+	
+	var body = '';
+	var last = '';
+	req.on('data', function(chunk){
+		//console.log("Chunk111111111111111111111111: " + chunk);
+		last = chunk;
+		body+=chunk;
+	});
+
+	req.on('end', function(){
+		var filename = 'test.tiff';
+		//filename = 'test.txt';
+        	fs.writeFile(filename, body , function(err){
+                		if (err) throw err;
+                		//console.log("Lastin: " + last);
+				
+				var junkIndex = last.indexOf("--");
+				var junkStr = last.substring(junkIndex);
+				console.log("JUNK!: " + junkStr);
+				body.replace(junkstr, '');
+				//console.log(body);
+
+				var command = 'tesseract /Github/Github/PhotoDIstance-PhotoEquation/server_side/test.tiff /Github/Github/PhotoDIstance-PhotoEquation/server_side/test -psm 7';
+				//command = 'ls';
+                		child = exec(command, function(error, stdout, stderr){
+                        		if (error) throw error;
+
+                        		//console.log('stdout: ' + stdout);
+                        		//console.log('stderr: ' + stderr);
+
+					fs.readFile('test.txt', function(err,str){
+						if (err) throw err;
+						console.log('File: ' +str);
+						
+						//call wolfram to do the math
+						wolfram.query("", function(err, result){
+							if (err) throw err;
+							console.log("Result: ");
+						});
+						
+						res.writeHeader(200, {"Content-Type":"application/json"});
+        					//res.write();
+						var result = {
+							eq: '1+1',
+							ans: 2
+						};
+						res.write(JSON.stringify(result));
+        					//res.end(JSON.stringify(result));
+						res.end();
+						console.log("SENT RESPONSE!");
+					});
+                		});
+        	});
+	});
+
+}).listen(8988);
